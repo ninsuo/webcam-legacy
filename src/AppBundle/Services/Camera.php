cd4d107dac3a09dffc347f113b0e2f7421207f06
alain tiemblo <ninsuo@gmail.com>
2018-08-04 20:52:40 +0200

    moving the time a bit right because there is no more the utc stuff

--- src/AppBundle/Services/Camera.php
+++ src/AppBundle/Services/Camera.php
@@ -15,10 +15,10 @@ class Camera extends BaseService
     const DEFAULT_WIDTH     = 1280;
     const DEFAULT_HEIGHT    = 720;
     const DEFAULT_TIME_SIZE = 28;
-    const DEFAULT_TIME_X    = 835;
+    const DEFAULT_TIME_X    = 920;
     const DEFAULT_TIME_Y    = 700;
 
-    public function __construct()
+    public function setRightTimezone()
     {
         date_default_timezone_set($this->getParameter('timezone'));
     }
